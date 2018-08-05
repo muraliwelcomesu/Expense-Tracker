17abd225437525c5d5c22cbbb34e2795a3f0ddbd
muraliwelcomesu <muraliwelcomesu@gmail.com>
2018-08-05 20:03:12 +0530

    version_1.0

---
+++ User.py
@@ -0,0 +1,5 @@
+#Declare the Global variables for user id and passwords.
+Password_Excel = 'C:\\Passwords.xlsx'
+Exp_MasterRec_sheetName = 'Bills' 
+Expense_Track_Path = 'C:\\Python\\'
+Expense_Track_ExcelName = 'Bills.xlsx'
